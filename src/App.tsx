d2048032d7ad639cf56ced8cd96f8a6a2a9ff976
pedrolauro15 <pedrolauro15@gmail.com>
2020-08-01 14:30:49 -0300

    notification tet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './global.css';
 
 function App() {
+  const [notificationText, setNotificationText] = useState('');
+
+  function notifyMe() {
+    if (!("Notification" in window)) {
+      alert("Este browser não suporta notificações de Desktop");
+    }
+    else if (Notification.permission === "granted") {
+      var notification = new Notification("minha notificação!", {
+        body: notificationText,
+        image: "https://www.rbsdirect.com.br/imagesrc/25633494.jpg?w=150",
+        icon: "https://www.rbsdirect.com.br/imagesrc/25633494.jpg?w=150",
+      });
+      notification.onclose = () => {
+        alert("Bye");
+      };
+    }
+
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        if (permission === "granted") {
+          var notification = new Notification("Hi there!");
+          notification.onclick = () => {alert("Hello")}
+        }
+      });
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notification test</h1>
+      <input
+        type="text"
+        name=""
+        id="NotInput"
+        placeholder="Notificação"
+        value={notificationText}
+        onChange={(e) => setNotificationText(e.target.value)}
+      />
+      <button onClick={notifyMe}>Enviar notificação</button>
     </div>
   );
 }
